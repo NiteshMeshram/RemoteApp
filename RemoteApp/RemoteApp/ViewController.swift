ba5f20891e4f5eb2177d15d146842446cd8b296a
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2017-08-06 22:09:59 +0530

    Added the iOS application

---
+++ RemoteApp/RemoteApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RemoteApp
+//
+//  Created by NItesh on 06/08/17.
+//  Copyright (c) 2017 NItesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
